d1f8ea93a0d25d29035ceadc068433c263a5c93e
prabha <cprabha685@gmail.com>
2021-09-24 19:09:23 +0530

    day5 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:catalog_app/pages/home_page.dart';
 import 'package:catalog_app/pages/login_page.dart';
+import 'package:catalog_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 void main() {
@@ -11,15 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
+    //  debugShowCheckedModeBanner:false, //if you want to make invisible debug banner,use this false...
        themeMode: ThemeMode.light,
        theme:ThemeData(primarySwatch: Colors.deepPurple,
      fontFamily: GoogleFonts.lato().fontFamily),
        darkTheme:ThemeData(brightness: Brightness.dark),
      // home:const HomePage(),
-     initialRoute: "/login",
+     initialRoute: "/",
      routes: {
-       "/":(context)=>const HomePage(),
-       "/login":(context)=> const LoginPage()
+       "/":(context)=>const LoginPage(),
+       MyRoutes.homeroute:(context)=>const HomePage(),
+       MyRoutes.loginroute:(context)=> const LoginPage()
      },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,8 @@
+import 'package:catalog_app/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 class LoginPage extends StatelessWidget {
   const LoginPage({ Key? key }) : super(key: key);
 
@@ -9,53 +10,57 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Material(
       color: Colors.white,
-    child: Column(
-      children: [
-        Image.asset("assets/images/login.png",
-        fit: BoxFit.cover),
-        const SizedBox(
-          height: 20,
-        ),
-        const Text("Welcome",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.indigo
-          
-        )),
-       const SizedBox(
-          height: 20,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText:"Enter User Name",
-                  labelText: "User Name"
-                ),
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: const InputDecoration(
-                  hintText:"Enter Password",
-                  labelText: "Password"
-                ),
-              ),
-               const SizedBox(
-                height: 20.0,
-              ),
-              ElevatedButton(
-                onPressed:(){
-                // ignore: avoid_print
-                print("hi prabha!");
-              } ,
-               child: const Text("Login"))
-            ],
+    child: SingleChildScrollView(
+      child: Column(
+        children: [
+          Image.asset("assets/images/login.png",
+          fit: BoxFit.cover),
+          const SizedBox(
+            height: 40,
           ),
-        )
-      ],
+          const Text("Welcome",
+          style: TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+            color: Colors.indigo
+            
+          )),
+         const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    hintText:"Enter User Name",
+                    labelText: "User Name"
+                  ),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    hintText:"Enter Password",
+                    labelText: "Password"
+                  ),
+                ),
+                 const SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  
+                 child: const Text("Login",style: TextStyle(fontSize: 18),),
+                 style: TextButton.styleFrom(minimumSize: const Size(150, 40)),
+                 onPressed:(){
+                  Navigator.pushNamed(context,MyRoutes.homeroute);
+                } ,
+                 )
+              ],
+            ),
+          )
+        ],
+      ),
     ),
       
     );
